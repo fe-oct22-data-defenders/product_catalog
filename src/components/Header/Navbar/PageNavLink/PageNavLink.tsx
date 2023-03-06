6a647c858a04a20d52531642b324aa7ce0f4d420
Oleksandr Nikora <mailnik@ukr.net>
2023-03-06 18:50:41 +0100

    try to fix menu

--- src/components/Header/Navbar/PageNavLink/PageNavLink.tsx
+++ src/components/Header/Navbar/PageNavLink/PageNavLink.tsx
@@ -6,9 +6,11 @@ import { NavLink } from 'react-router-dom';
 export type Props = {
   to: string;
   text: string;
+  // eslint-disable-next-line react/no-unused-prop-types
+  setMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
 };
 
-export const PageNavLink: FC<Props> = ({ to, text }) => (
+export const PageNavLink: FC<Props> = ({ to, text, setMenuOpen }) => (
   <NavLink
     className={({ isActive }) => (
       cn('navbar__menu-list-link', {
@@ -16,14 +18,16 @@ export const PageNavLink: FC<Props> = ({ to, text }) => (
       })
     )}
     to={to}
+    onClick={() => setMenuOpen(false)}
   >
     <div className="navbar__menu-list-link-container">{text}</div>
   </NavLink>
 );
 
-export const NavbarMenuLink: React.FC<Props> = ({ to, text }) => {
+export const MenuLink: React.FC<Props> = ({ to, text, setMenuOpen }) => {
   return (
     <NavLink
+      onClick={() => setMenuOpen(false)}
       to={to}
       className={({ isActive }) => cn('menu__link', { 'is-active': isActive })}
     >
